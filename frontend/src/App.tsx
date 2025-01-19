48deefe76e7d3b02c5bb9080da3b0665871bb47b
Jak Akdemir <jakdemir@jakdemir@gmail.com>
2025-01-19 11:50:01 +0300

    analytics + contacts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,26 @@ import ChildrenForm from './components/forms/ChildrenForm';
 import ParentsForm from './components/forms/ParentsForm';
 import GrandparentsForm from './components/forms/GrandparentsForm';
 import Layout from './components/Layout';
+import Footer from './components/Footer';
 import { FamilyTreeProvider } from './context/FamilyTreeContext';
 
 function App() {
   return (
     <FamilyTreeProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<SpouseForm />} />
-            <Route path="children" element={<ChildrenForm />} />
-            <Route path="parents" element={<ParentsForm />} />
-            <Route path="grandparents" element={<GrandparentsForm />} />
-          </Route>
-        </Routes>
+        <div className="min-h-screen flex flex-col">
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<SpouseForm />} />
+                <Route path="children" element={<ChildrenForm />} />
+                <Route path="parents" element={<ParentsForm />} />
+                <Route path="grandparents" element={<GrandparentsForm />} />
+              </Route>
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </Router>
     </FamilyTreeProvider>
   );
